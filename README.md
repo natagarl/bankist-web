d4af63a8c79417acd307f2f68327ebd76c1e0f7c
Natasha Garlukovich <natali.garlukovich@gmail.com>
2022-04-29 08:22:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bankist website
+
+🌱 in progress
+
+Study project on [_The Complete JavaScript Course 2022_](https://www.udemy.com/course/the-complete-javascript-course/) by [_Jonas Schmedtmann_](https://www.udemy.com/user/jonasschmedtmann/).
+
+## Descriprion
+
+A website for an imaginary bank.
---
+++ script.js
@@ -0,0 +1,203 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+/////////// LECTURES /////////////
+
+///////////////////////////////
+// 185. How the DOM Really Works
+
+///////////////////////////////
+// 186. Selecting, Creating and Deleting Elements
+/*
+
+// Selecting elements 
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete alaments
+document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+  message.remove();
+  // how it was before - an old way
+  // message.parentElement.removeChild(message);
+})
+
+
+////////////////////////////////////////
+// 187. Styles, Attributes and Classes
+
+// Styles
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+console.log(message.style.color);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 +'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standart
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+const link2 = document.querySelector('.nav__link--btn');
+console.log(link2.href);
+console.log(link2.getAttribute('href'));
+
+// Data attributes
+
+console.log(logo.dataset.versionNumber);
+
+// Classes
+
+logo.classList.add('c', 'j');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// don't use - it will ovewrite all classes
+logo.className = 'jonas';
+
+*/
+////////////////////////////////
+// 188. Implementing Smooth Scrolling
+/*
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  // console.log('height/width vieport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // Scrolling
+  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // })
+
+  // Modern way
+  section1.scrollIntoView({behavior: 'smooth'});
+});
+
+*/
+
+/////////////////////////////////
+// 189. Types of Events and Event Handlers
+
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function(e) {
+//   alert('addEventListener: Creat! You are reading the heading :D')
+//   h1.removeEventListener('mouseenter', alertH1);
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
+
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// // old school
+// h1.onmouseenter = function(e) {
+//   alert('onmouseenter: Creat! You are reading the heading :D')
+// };
+
+////////////////////////////////
+// 190. Event Propagation: Bubbling and Capturing
+
+////////////////////////////////
+// 191. Event Propagation in Practice
+
+// rgb(255,255,255)
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+  console.log('Link');
+});
