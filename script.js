bf03bf339e683b03e92612c8c39dc9d38f184d37
Natasha Garlukovich <natali.garlukovich@gmail.com>
2022-05-05 10:38:04 +0200

    last lectures

--- script.js
+++ script.js
@@ -547,3 +547,23 @@ console.log(h1.parentElement.children);
 
 //////////////////////////////////////
 // 194. Building a Tabbed Component
+
+
+///////////////////////////////////
+// Lifecycle DOM events
+
+// DOM content loaded
+document.addEventListener('DOMContentLoaded', function(e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+
+window.addEventListener('load', function(e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function(e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
