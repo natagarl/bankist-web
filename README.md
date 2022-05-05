0c21b7b4843922586445ec07e882d63a474ee847
Natasha Garlukovich <natali.garlukovich@gmail.com>
2022-05-05 11:24:21 +0200

    cleaning comments and updating readme

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# Bankist website
-
-🌱 in progress
+# [Bankist website](https://natagarl.github.io/bankist-web/)
 
 Study project on [_The Complete JavaScript Course 2022_](https://www.udemy.com/course/the-complete-javascript-course/) by [_Jonas Schmedtmann_](https://www.udemy.com/user/jonasschmedtmann/).
 
 ## Descriprion
 
-A website for an imaginary bank.
+A website for an imaginary bank. It's a non-responsive website with some animations written in JavaScript.
+
+## How it was made
+
+_HTML_ and _CSS_ code was written by the teacher. I wrote _JavaScript_ code while watching tutorials from the course.
--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 
-///////////////////////////////////////
+
 // Modal window
 
 const openModal = function (e) {
@@ -36,50 +36,18 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-////////////////////////////////////////////
+
 // Button scrolling
 
 btnScrollTo.addEventListener('click', function (e) {
-  // const s1coords = section1.getBoundingClientRect();
-  // console.log(s1coords);
-
-  // console.log(e.target.getBoundingClientRect());
-  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
-
-  // console.log('height/width vieport', document.documentElement.clientHeight, document.documentElement.clientWidth);
-
-  // Scrolling
-  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
-
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // })
-
-  // Modern way
-  section1.scrollIntoView({behavior: 'smooth'});
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-////////////////////////////////////////
+
 // Page navigation
-
-// document.querySelectorAll('.nav__link').forEach(function(el) {
-//   el.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     const id = this.getAttribute('href');
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
-//   })
-// });
-
-// 1. Add event listener to common parent element
-// 2. Determine what elemet originated the event
 
 document.querySelector('.nav__links').addEventListener('click', function(e) {
   e.preventDefault();
-
-  // Matching strategy
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
     console.log(id);
@@ -87,27 +55,18 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
   }
 });
 
+
 // Tabbed component
 
 tabsContainer.addEventListener('click', function(e) {
   const clicked = e.target.closest('.operations__tab');
-
-  // Guard clause
   if (!clicked) return;
-
-  // More old way of guard clause
-  // if (clicked) {}
-
-  // Remove active classes
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
-
-  // Activate tab
   clicked.classList.add('operations__tab--active');
-
-  // Activate content area
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
+
 
 // Menu fade animation
 
@@ -128,39 +87,14 @@ const handleHover = function(e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
-////////////////////////////
+
 // Sticky navigation
-// const initialCoords = section1.getBoundingClientRect();
-// console.log(initialCoords);
-// window.addEventListener('scroll', function() {
-//   console.log(window.scrollY);
-//   if(window.scrollY > initialCoords.top) nav.classList.add('sticky');
-//   else nav.classList.remove('sticky');
-// });
-
-// Sticky navigation: Intersection Observer API
-
-// const obsCallback = function(entries, observer) {
-//   entries.forEach(entry => {
-//     console.log(entry);
-//   });
-// };
-
-// const obsOptions = {
-//   root: null,
-//   threshold: [0, 0.2],
-// };
-
-// const observer = new IntersectionObserver(obsCallback, obsOptions);
-// observer.observe(section1);
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-// console.log(navHeight);
 
 const stickyNav = function(entries) {
   const [entry] = entries;
-  // console.log(entry);
 
   if(!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -172,19 +106,14 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
-/////////////////////////////
-// Revealing elements on scroll
 
-// Reveal sections
+// Revealing sections on scroll
 
 const allSections = document.querySelectorAll('.section');
 
 const revealSection = function(entries, observer) {
   const [entry] = entries;
-  // console.log(entry);
-
   if(!entry.isIntersecting) return;
-
   entry.target.classList.remove('section--hidden');
   observer.unobserve(entry.target);
 }
@@ -196,21 +125,17 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function(section) {
   sectionObserver.observe(section);
-  // section.classList.add('section--hidden');
+  section.classList.add('section--hidden');
 })
 
-/////////////////////////////////
+
 // Lazy loading images
 
 const imgTargets = document.querySelectorAll('img[data-src]');
 
 const loadImg = function (entries, observer) {
   const [entry] = entries;
-  // console.log(entry);
-
   if (!entry.isIntersecting) return;
-
-  // Replace src with data-src
   entry.target.src = entry.target.dataset.src;
   entry.target.addEventListener('load', function () {
     entry.target.classList.remove('lazy-img');
@@ -227,7 +152,6 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach(img => imgObserver.observe(img));
 
 
-///////////////////////////////////
 // Building a slider component
 
 // Slider
@@ -236,7 +160,6 @@ const slider = function() {
   const btnLeft = document.querySelector('.slider__btn--left');
   const btnRight = document.querySelector('.slider__btn--right');
   const dotContainer = document.querySelector('.dots');
-
   let curSlide = 0;
   const maxSlide = slides.length;
 
@@ -261,8 +184,6 @@ const slider = function() {
     slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
   };
 
-  // Next slide
-
   const nextSlide = function() {
     if(curSlide === maxSlide - 1) {
       curSlide=0;
@@ -281,14 +202,19 @@ const slider = function() {
     }
     goToSlide(curSlide);
     activateDot(curSlide);
-  }
+  };
+
   const init = function() {
     createDots();
     goToSlide(0);
     activateDot(0);
   };
+
   init();
+
+
   // Event handlers
+
   btnRight.addEventListener('click', nextSlide);
   btnLeft.addEventListener('click', prevSlide);
 
@@ -310,260 +236,3 @@ const slider = function() {
 };
 
 slider();
-
-// currSlide = 1;
-
-/////////// LECTURES /////////////
-
-///////////////////////////////
-// 185. How the DOM Really Works
-
-///////////////////////////////
-// 186. Selecting, Creating and Deleting Elements
-/*
-
-// Selecting elements
-
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
-
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
-
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
-
-console.log(document.getElementsByClassName('btn'));
-
-
-// Creating and inserting elements
-// .insertAdjacentHTML
-
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-// message.textContent = 'We use cookies for improved functionality and analytics';
-message.innerHTML = 'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>';
-
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
-
-// header.before(message);
-// header.after(message);
-
-// Delete alaments
-document.querySelector('.btn--close-cookie').addEventListener('click', function() {
-  message.remove();
-  // how it was before - an old way
-  // message.parentElement.removeChild(message);
-})
-
-
-////////////////////////////////////////
-// 187. Styles, Attributes and Classes
-
-// Styles
-
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-
-console.log(message.style.height);
-console.log(message.style.backgroundColor);
-console.log(message.style.color);
-
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
-
-message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 +'px';
-
-document.documentElement.style.setProperty('--color-primary', 'orangered');
-
-// Attributes
-
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
-
-logo.alt = 'Beautiful minimalist logo';
-
-// Non-standart
-console.log(logo.designer);
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('company', 'Bankist');
-
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
-
-const link = document.querySelector('.twitter-link');
-console.log(link.href);
-console.log(link.getAttribute('href'));
-
-const link2 = document.querySelector('.nav__link--btn');
-console.log(link2.href);
-console.log(link2.getAttribute('href'));
-
-// Data attributes
-
-console.log(logo.dataset.versionNumber);
-
-// Classes
-
-logo.classList.add('c', 'j');
-logo.classList.remove('c');
-logo.classList.toggle('c');
-logo.classList.contains('c'); // not includes
-
-// don't use - it will ovewrite all classes
-logo.className = 'jonas';
-
-*/
-////////////////////////////////
-// 188. Implementing Smooth Scrolling
-/*
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
-btnScrollTo.addEventListener('click', function (e) {
-  // const s1coords = section1.getBoundingClientRect();
-  // console.log(s1coords);
-
-  // console.log(e.target.getBoundingClientRect());
-  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
-
-  // console.log('height/width vieport', document.documentElement.clientHeight, document.documentElement.clientWidth);
-
-  // Scrolling
-  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
-
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // })
-
-  // Modern way
-  section1.scrollIntoView({behavior: 'smooth'});
-});
-
-*/
-
-/////////////////////////////////
-// 189. Types of Events and Event Handlers
-
-// const h1 = document.querySelector('h1');
-
-// const alertH1 = function(e) {
-//   alert('addEventListener: Creat! You are reading the heading :D')
-//   h1.removeEventListener('mouseenter', alertH1);
-// };
-
-// h1.addEventListener('mouseenter', alertH1);
-
-// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
-
-// // old school
-// h1.onmouseenter = function(e) {
-//   alert('onmouseenter: Creat! You are reading the heading :D')
-// };
-
-////////////////////////////////
-// 190. Event Propagation: Bubbling and Capturing
-
-////////////////////////////////
-// 191. Event Propagation in Practice
-
-/*
-
-// rgb(255,255,255)
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-console.log(randomColor());
-
-document.querySelector('.nav__link').addEventListener('click', function(e) {
-  this.style.backgroundColor = randomColor();
-  console.log('LINK', e.target, e.currentTarget);
-  console.log(e.currentTarget === this);
-
-  // Stop propagation
-  // e.stopPropagation();
-});
-
-document.querySelector('.nav__links').addEventListener('click', function(e) {
-  this.style.backgroundColor = randomColor();
-  console.log('CONTAINER', e.target, e.currentTarget);
-});
-
-document.querySelector('.nav').addEventListener('click', function(e) {
-  this.style.backgroundColor = randomColor();
-  console.log('NAV', e.target, e.currentTarget);
-});
-
-*/
-
-
-//////////////////////////////////////
-// 192. Event Delegation: Implementing Page Navigation
-
-////////////////////////////////////
-// 193. Dom Traversing
-
-/*
-
-const h1 = document.querySelector('h1');
-
-// Going downwards: child
-console.log(h1.querySelectorAll('.highlight'));
-console.log(h1.childNodes);
-console.log(h1.children);
-h1.firstElementChild.style.color = 'white';
-h1.lastElementChild.style.color = 'orangered';
-
-// Going upwards: parents
-console.log(h1.parentNode);
-console.log(h1.parentElement);
-
-h1.closest('.header').style.background = 'var(--gradient-secondary)';
-h1.closest('h1').style.background = 'var(--gradient-primary)';
-
-
-// Going sideways: siblings
-console.log(h1.previousElementSibling);
-console.log(h1.nextElementSibling);
-
-console.log(h1.previousSibling);
-console.log(h1.nextSibling);
-
-console.log(h1.parentElement.children);
-[...h1.parentElement.children].forEach(function(el) {
-  if (el !== h1) el.style.transform = 'scale(0.5)';
-})
-
-*/
-
-
-//////////////////////////////////////
-// 194. Building a Tabbed Component
-
-
-///////////////////////////////////
-// Lifecycle DOM events
-
-// DOM content loaded
-document.addEventListener('DOMContentLoaded', function(e) {
-  console.log('HTML parsed and DOM tree built!', e);
-});
-
-
-window.addEventListener('load', function(e) {
-  console.log('Page fully loaded', e);
-});
-
-// window.addEventListener('beforeunload', function(e) {
-//   e.preventDefault();
-//   console.log(e);
-//   e.returnValue = '';
-// });
